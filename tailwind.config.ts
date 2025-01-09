6c401f7682ba6d48a74ed7f845d56134a1588a15
Woohyun <gusdn0481@gmail.com>
2025-01-09 17:53:32 +0900

    ✨ feat : add custom animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ module.exports = {
       colors: {
         main: '#26B7BC',
       },
+      animation: {
+        'bounce-updown': 'bounce-updown 1s infinite ease-in-out',
+      },
+      keyframes: {
+        'bounce-updown': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+      },
     },
   },
   plugins: [],
