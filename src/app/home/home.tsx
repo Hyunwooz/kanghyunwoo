8029c39f542e305cb16bcf645853be7b8a83a408
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-01-17 00:20:51 +0900

    ✨ feat : Experience 및 Education 컨텐츠 추가

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -130,28 +130,23 @@ const Home = () => {
         <p className='ml-1 animate-blink'>{'|'}</p>
       </div>
       <div
-        className={`z-10 mb-[40px] items-center justify-center px-4 pt-12 text-start transition-all duration-1000 md:fixed md:top-0 md:w-[44%] md:pl-[60px] md:pt-0 lg:pl-[100px] xl:pl-[160px] 2xl:pl-[270px] ${
+        className={`z-10 mb-[40px] items-center justify-center px-4 pt-12 text-start transition-all duration-1000 md:fixed md:top-0 md:w-[50%] md:pl-[60px] md:pt-0 lg:pl-[100px] xl:w-[44%] xl:pl-[160px] 2xl:pl-[270px] ${
           isClicked
             ? 'translate-y-0 opacity-100 md:translate-y-[64px] lg:translate-y-[78px] xl:translate-y-[94px]'
             : 'translate-y-[100%] opacity-0 md:translate-y-[210px]'
         }`}
       >
-        <h1 className='mb-3 text-5xl font-bold md:text-[60px] xl:mb-6 xl:text-[88px]'>
+        <h1 className='mb-2 text-5xl font-bold md:text-[60px] xl:mb-4 xl:text-[88px]'>
           강현우
         </h1>
-        {/* <p className='mb-4 text-lg font-semibold xl:text-[36px]'>
-            FRONT-END DEVELOPER
-          </p> */}
-        <p className='text-md mb-1 font-medium leading-6 md:mb-2 xl:text-[18px]'>
-          사용자 경험을 중심으로 직관적이고 창의적인 웹 서비스를 만드는
-          프론트엔드의 매력에 빠져있습니다.
+        <p className='text-md mb-2 font-medium leading-6 text-[#ffffffcb] md:mb-3 xl:text-[18px]'>
+          사용자의 피드백을 즉각적으로 반영하여 웹 애플리케이션에 가치를 더하는
+          과정에서 큰 보람을 느낍니다.
         </p>
-        <p className='text-md mb-8 font-medium leading-6 md:mb-6 xl:text-[18px]'>
-          사용자의 피드백을 즉각적으로 반영하여여 웹 애플리케이션에 가치를
-          더하는 과정에서 큰 보람을 느낍니다.
-        </p>
-        <p className='text-md mb-10 font-medium md:mb-12 xl:text-[18px]'>
-          이 사이트를 방문해 주셔서 진심으로 감사드립니다.
+        <p className='text-md mb-12 font-medium leading-6 text-[#ffffffcb] md:mb-24 xl:text-[18px]'>
+          사용자 경험을 최적화하고, 인터페이스를 직관적으로 만드는{' '}
+          <strong className='font-extrabold text-white'>프론트엔드</strong>를
+          담당하고 있습니다. 제 사이트를 방문해 주셔서 진심으로 감사드립니다.
         </p>
         {sectionList.map((section, index) => (
           <div
@@ -172,25 +167,25 @@ const Home = () => {
             href={'https://github.com/hyunwooz'}
             className='duration-400 opacity-60 transition-transform hover:scale-125 hover:opacity-100'
           >
-            <Icon icon='akar-icons:github-fill' width={26} height={26} />
+            <Icon icon='akar-icons:github-fill' width={30} height={30} />
           </Link>
           <Link
             href={'https://www.instagram.com/wooh.dev/'}
             className='duration-400 opacity-60 transition-transform hover:scale-125 hover:opacity-100'
           >
-            <Icon icon='ri:instagram-fill' width={26} height={26} />
+            <Icon icon='ri:instagram-fill' width={30} height={30} />
           </Link>
           <Link
             href={'https://www.linkedin.com/in/woohyundev/'}
             className='duration-400 opacity-60 transition-transform hover:scale-125 hover:opacity-100'
           >
-            <Icon icon='pajamas:linkedin' width={26} height={26} />
+            <Icon icon='pajamas:linkedin' width={30} height={30} />
           </Link>
         </div>
       </div>
       <section
         id='About'
-        className='relative min-h-[80vh] w-full pb-10 pt-8 md:min-h-screen md:pb-[80px] md:pt-[50px]'
+        className='relative w-full pb-2 pt-8 md:pb-[80px] md:pt-[50px]'
       >
         <div
           className={`flex flex-col items-start justify-center text-start text-sm transition-all duration-1000 md:items-end ${
@@ -201,15 +196,15 @@ const Home = () => {
         >
           <div className='px-0 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
             <h2
-              className={`sticky top-0 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
+              className={`sticky top-0 z-10 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
             >
               About
             </h2>
             <div className='px-4 sm:p-0'>
-              <h3 className='mb-4 bg-light px-2 py-2 text-[18px] font-semibold text-deep md:text-[20px] xl:text-[24px]'>
-                [ 프론트엔드 개발 ]
+              <h3 className='mb-4 rounded bg-light px-2 py-3 text-[22px] font-semibold text-deep md:text-[24px] xl:text-[28px]'>
+                프론트엔드 개발
               </h3>
-              <p className='mb-6 font-light leading-6 md:mb-12 xl:text-[18px]'>
+              <p className='mb-6 text-base font-normal leading-6 md:mb-12 xl:text-[18px]'>
                 비전공자로 웹 개발 공부를 시작한 후, 현재는 프론트엔드 개발을
                 담당하고 있습니다. 처음에는 Python을 활용해 반복적인 업무를
                 자동화하며 개발에 흥미를 느꼈고, 그 후 프론트엔드 분야로 관심을
@@ -217,10 +212,10 @@ const Home = () => {
                 직관적이고 효율적인 웹 인터페이스를 구현하는 것에 큰 보람을
                 느끼고 있습니다.
               </p>
-              <h3 className='mb-4 rounded bg-light px-2 py-2 text-[18px] font-semibold text-deep md:text-[20px] xl:text-[24px]'>
-                [ 성장에 대한 즐거움 ]
+              <h3 className='mb-4 rounded bg-light px-2 py-3 text-[22px] font-semibold text-deep md:text-[24px] xl:text-[28px]'>
+                성장에 대한 즐거움
               </h3>
-              <p className='mb-6 font-light leading-6 md:mb-12 xl:text-[18px]'>
+              <p className='mb-6 text-base font-normal leading-6 md:mb-12 xl:text-[18px]'>
                 새로운 프로젝트를 시작할 때마다 단순히 이전에 사용하던 기술을
                 활용하는 것이 아닌, 이전 프로젝트에서 아쉬웠던 부분을 개선하고
                 새로운 코드 스타일이나 기술을 적용하는 것에 큰 즐거움을
@@ -228,10 +223,10 @@ const Home = () => {
                 점점 더 나은 개발자로 나아가는 과정에서 큰 성취감을 얻고
                 있습니다.
               </p>
-              <h3 className='mb-4 rounded bg-light px-2 py-2 text-[18px] font-semibold text-deep md:text-[20px] xl:text-[24px]'>
-                [ 안되면 될 때까지 ]
+              <h3 className='mb-4 rounded bg-light px-2 py-3 text-[22px] font-semibold text-deep md:text-[24px] xl:text-[28px]'>
+                안되면 될 때까지
               </h3>
-              <p className='font-light leading-6 xl:text-[18px]'>
+              <p className='text-base font-normal leading-6 xl:text-[18px]'>
                 프론트엔드 개발을 혼자 진행하고 있습니다. 이 과정에서 모든 것을
                 스스로 찾아내고 해결해야 했기 때문에, 때론 기능 구현에 며칠이
                 걸리기도 했습니다. 하지만 이런 과정을 통해 개발에 있어서{' '}
@@ -243,10 +238,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section
-        id='Skills'
-        className='relative min-h-[80vh] w-full md:h-[100vh]'
-      >
+      <section id='Skills' className='relative w-full pb-2 md:pb-[40px]'>
         <div
           className={`flex flex-col items-start justify-center text-start text-sm font-medium transition-all duration-1000 md:items-end ${
             isClicked
@@ -254,9 +246,9 @@ const Home = () => {
               : 'translate-y-[30%] opacity-0'
           }`}
         >
-          <div className='pt-10 sm:pt-0 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
+          <div className='w-full pt-6 sm:pt-0 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
             <h2
-              className={`sticky top-0 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
+              className={`sticky top-0 z-10 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
             >
               Skills
             </h2>
@@ -266,10 +258,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section
-        id='Experience'
-        className='relative h-[100vh] min-h-[80vh] w-full md:h-[100vh]'
-      >
+      <section id='Experience' className='relative w-full pb-2 md:pb-[40px]'>
         <div
           className={`flex flex-col items-start justify-center text-start text-sm font-medium transition-all duration-1000 md:items-end ${
             isClicked
@@ -277,35 +266,98 @@ const Home = () => {
               : 'translate-y-[30%] opacity-0'
           }`}
         >
-          <div className='pt-10 sm:pt-0 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
+          <div className='w-full pt-6 sm:pt-0 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
             <h2
-              className={`sticky top-0 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
+              className={`sticky top-0 z-10 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
             >
               Experience
             </h2>
             <div className='px-4 sm:p-0'>
-              <p className='mb-6 md:mb-4 xl:text-[18px]'>
-                {`I’m a developer passionate about crafting accessible, pixel-perfect
-            user interfaces that blend thoughtful design with robust
-            engineering. My favorite work lies at the intersection of design and
-            development, creating experiences that not only look great but are
-            meticulously built for performance and usability.`}
-              </p>
-              <p className='mb-6 md:mb-4 xl:text-[18px]'>
-                {`Currently, I'm a
-            Senior Front-End Engineer at Klaviyo, specializing in accessibility.
-            I contribute to the creation and maintenance of UI components that
-            power Klaviyo’s frontend, ensuring our platform meets web
-            accessibility standards and best practices to deliver an inclusive
-            user experience. `}
-              </p>
+              <div>
+                <Link
+                  href={'https://s-g.kr'}
+                  className='mb-1 block border-b pb-2 text-2xl font-semibold hover:bg-light hover:bg-opacity-10 md:text-3xl'
+                >
+                  에스엔지컴퍼니
+                </Link>
+                <p className='mb-2 text-base font-light md:mb-4 md:text-lg'>
+                  테크기반 마케팅 및 프롭테크 서비스 스타트업
+                </p>
+                <div className='flex flex-col gap-4 md:flex-row md:gap-0'>
+                  <div className='flex w-full items-center gap-4 md:block md:w-2/6'>
+                    <h4 className='text-base font-semibold md:mb-2 md:text-lg'>
+                      프론트엔드 개발자
+                    </h4>
+                    <p className='text-sm font-normal md:text-base'>
+                      2023.11 ~ 현재
+                    </p>
+                  </div>
+                  <div className='w-full md:w-4/6 md:px-2'>
+                    <ul className='flex list-inside list-disc flex-col gap-1'>
+                      <li>개발 업무 프로세스 구축 및 프로젝트 문서화 진행</li>
+                      <li>CI/CD 및 Git Flow 적용 등 개발 환경 개선</li>
+                      <li>노션을 활용한 업무 프로세스 ( 칸반보드 ) 도입</li>
+                      <li>파이썬을 활용한 다양한 업무 자동화</li>
+                      <li>계약서 자동 관리 시스템 구축</li>
+                      <li>자사 서비스 통합 관리 서비스 구축 및 유지보수</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id='Education' className='relative w-full pb-2 md:pb-[40px]'>
+        <div
+          className={`flex flex-col items-start justify-center text-start text-sm font-medium transition-all duration-1000 md:items-end ${
+            isClicked
+              ? 'mb-[60px] opacity-100 sm:translate-y-[96px]'
+              : 'translate-y-[30%] opacity-0'
+          }`}
+        >
+          <div className='w-full pt-6 sm:pt-0 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
+            <h2
+              className={`sticky top-0 z-10 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
+            >
+              Education
+            </h2>
+            <div className='px-4 sm:p-0'>
+              <div>
+                <Link
+                  href={'https://estfamily.career.greetinghr.com/kdt'}
+                  className='mb-1 block border-b pb-2 text-2xl font-semibold hover:bg-light hover:bg-opacity-10 md:text-3xl'
+                >
+                  [ESTsoft] 백엔드 개발 오르미 1기
+                </Link>
+                <p className='mb-2 text-base font-light md:mb-4 md:text-lg'>
+                  ESTsoft 주관 백엔드 개발자 과정 1기 부트캠프
+                </p>
+                <div className='flex flex-col gap-4 md:flex-row md:gap-0'>
+                  <div className='flex w-full items-center gap-4 md:block md:w-2/6'>
+                    <p className='text-sm font-normal md:text-base'>
+                      2023.04 ~ 2023.09
+                    </p>
+                  </div>
+                  <div className='w-full md:w-4/6 md:px-2'>
+                    <ul className='flex list-inside list-disc flex-col gap-1'>
+                      <li>GitHub, Python, Django, Django Restframe Work</li>
+                      <li>ERD, PostgreSQL, HTML, CSS, JaveScripts</li>
+                      <li>Django 웹 프레임워크 기반 프로젝트 3건 제작</li>
+                      <li>Django 개인 프로젝트 최우수상 수상</li>
+                      <li>Django 팀 프로젝트 최우수상 수상</li>
+                      <li>23년 7월 이달의 오르미 선정</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section
         id='Projects'
-        className='relative h-[100vh] min-h-[80vh] w-full md:h-[100vh]'
+        className='relative min-h-[60vh] w-full pb-2 md:pb-[40px]'
       >
         <div
           className={`flex flex-col items-start justify-center text-start text-sm font-medium transition-all duration-1000 md:items-end ${
@@ -314,26 +366,41 @@ const Home = () => {
               : 'translate-y-[30%] opacity-0'
           }`}
         >
-          <div className='pt-10 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
+          <div className='pt-6 md:mt-0 md:w-5/12 md:pr-[60px] lg:pr-[100px] xl:pr-[160px] 2xl:pr-[270px]'>
             <h2
-              className={`sticky top-0 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
+              className={`sticky top-0 z-10 mb-[30px] w-full bg-deep p-4 text-[32px] font-bold sm:static sm:p-0 md:mb-16 xl:text-[48px]`}
             >
               Projects
             </h2>
             <div className='px-4 sm:p-0'>
-              <p className='mb-10 md:mb-4 xl:text-[18px]'>
-                {`In the past, I've had the opportunity to develop
-                software across a variety of settings — from advertising agencies
-                and large corporations to start-ups and small digital product
-                studios. Additionally, I also released a comprehensive video course
-                a few years ago, guiding learners through building a web app with
-                the Spotify API.`}
-              </p>
-              <p className='xl:text-[18px]'>
-                {`In my spare time, I’m usually climbing, reading,
-                hanging out with my wife and two cats, or running around Hyrule
-                searching for Korok seeds K o r o k s e e d s .`}
-              </p>
+              <div>
+                <Link
+                  href={'https://estfamily.career.greetinghr.com/kdt'}
+                  className='mb-1 block border-b pb-2 text-2xl font-semibold hover:bg-light hover:bg-opacity-10 md:text-3xl'
+                >
+                  [ESTsoft] 백엔드 개발 오르미 1기
+                </Link>
+                <p className='mb-2 text-base font-light md:mb-4 md:text-lg'>
+                  ESTsoft 주관 백엔드 개발자 과정 1기 부트캠프
+                </p>
+                <div className='flex flex-col gap-4 md:flex-row md:gap-0'>
+                  <div className='flex w-full items-center gap-4 md:block md:w-2/6'>
+                    <p className='text-sm font-normal md:text-base'>
+                      2023.04 ~ 2023.09
+                    </p>
+                  </div>
+                  <div className='w-full md:w-4/6 md:px-2'>
+                    <ul className='flex list-inside list-disc flex-col gap-1'>
+                      <li>GitHub, Python, Django, Django Restframe Work</li>
+                      <li>ERD, PostgreSQL, HTML, CSS, JaveScripts</li>
+                      <li>Django 웹 프레임워크 기반 프로젝트 3건 제작</li>
+                      <li>Django 개인 프로젝트 최우수상 수상</li>
+                      <li>Django 팀 프로젝트 최우수상 수상</li>
+                      <li>23년 7월 이달의 오르미 선정</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
