cbd2171424feeb5fab94d5f816a7c10270403890
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-01-19 18:11:19 +0900

    💄 style : skill badge 배경색 변경

--- src/components/badge/skillBadge.tsx
+++ src/components/badge/skillBadge.tsx
@@ -2,7 +2,7 @@ export const ExperienceSkillBadge: React.FC<{ title: string }> = ({
   title,
 }) => {
   return (
-    <div className='text-background rounded-lg bg-main px-2 font-bold'>
+    <div className='text-background rounded-lg bg-light px-2 font-bold'>
       {title}
     </div>
   );
